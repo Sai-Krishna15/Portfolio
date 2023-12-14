36cb753e5126d92d74c1fb5d176a2bc412ebe6af
Sai-Krishna15 <138992231+Sai-Krishna15@users.noreply.github.com>
2023-12-14 13:27:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let progress=document.querySelectorAll('.progress span'),
+spans=document.querySelectorAll('.progress-title span');
+progress.forEach((prog)=>{
+    prog.style.width=prog.dataset.width;
+});
+spans.forEach((span)=>{
+    span.style.left=span.dataset.num;
+    span.style.opacity='1';
+    span.style.visibility='visible'
+});
+// change of names
+var type=new Typed("#multi-names",{
+    strings:["frontend developer","youtuber","web developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true,
+});
